174b56bf26e01ed3626de23819d3ca33a483a2bc
Spookysword <sword.spooky@gmail.com>
2022-06-12 13:01:49 -0700

    Add reytracing to demo

---
+++ demos/reytracing.c
@@ -0,0 +1,72 @@
+/*
+
+FAIR WARNING
+This demo is NOT finished, so do not expect much from it.
+
+*/
+#include <grey.h>
+
+#define size 12
+
+float playerX = 1280/2, playerY = 720/2, playerWidth = 25, playerHeight = 25, playerRotation = 0, rotationSpeed = 100, moveSpeed = 100;
+float circleX, circleY, circleR = 5;
+
+int main() {
+	initGrey(0);
+
+	Window win = createWindow(1280, 720, "greytracing");
+	
+	int x[size] = {
+		0, 0, 1280-50, 0, 200, 200, 200, 400, 400, 600, 600, 800
+	};
+	int y[size] = {
+		0, 0, 0, 720-50, 250, 250, 450, 450, 600, 450, 100, 100
+	};
+	int w[size] = {
+		1280, 50, 50, 1280, 250, 50, 250, 50, 200, 50, 50, 50
+	};
+	int h[size] = {
+		50, 720, 720, 50, 50, 250, 50, 200, 50, 200, 200, 550
+	};
+	
+	while (!shouldWindowClose(win)) {
+		updateWindow(&win);
+
+		clearWindowBackground(&win, COLOR_WHITE);
+
+		for (int i = 0; i < size; i++) {
+			drawRectangle(&win, x[i], y[i], w[i], h[i], 0, COLOR_BLACK);
+		}
+
+		float px = playerX + playerWidth, py = playerY + (playerHeight / 2);
+		float ox = playerX + (playerWidth / 2), oy = playerY + (playerHeight / 2);
+		float theta = playerRotation * (3.14159265359 / 180);
+
+		circleX = cos(theta) * (px - ox) - sin(theta) * (py - oy) + ox;
+		circleY = sin(theta) * (px - ox) + cos(theta) * (py - py) + oy;
+
+		// Controls
+		if (isKeyDown(win, KEY_LEFT)) {
+			playerRotation -= rotationSpeed * win.deltaTime;
+		}
+		if (isKeyDown(win, KEY_RIGHT)) {
+			playerRotation += rotationSpeed * win.deltaTime;
+		}
+		if (isKeyDown(win, KEY_UP)) {
+			playerX += (moveSpeed * cos(theta)) * win.deltaTime;
+			playerY += (moveSpeed * sin(theta)) * win.deltaTime;
+		}
+		if (isKeyDown(win, KEY_DOWN)) {
+			playerX -= (moveSpeed * cos(theta)) * win.deltaTime;
+			playerY -= (moveSpeed * sin(theta)) * win.deltaTime;
+		}
+
+		drawRectangle(&win, playerX, playerY, playerWidth, playerHeight, playerRotation, COLOR_YELLOW);
+		//drawCircle(&win, circleX, circleY, circleR, COLOR_RED);
+
+		renderWindow(win);
+	}
+
+	deleteWindow(&win);
+	closeGrey();
+}
--- main.c
+++ main.c
@@ -1,66 +1,311 @@
+/*
+
+FAIR WARNING
+This demo is NOT finished, so do not expect much from it.
+
+*/
 #include <grey.h>
+#include <stdio.h>
+#include <arey.h>
 
-#define size 12
+// Menu
+float resolutionDivider = 1.5;
+float width = 884, height = 1080;
+Color backgroundColor = { 40, 47, 51, 255 };
+Color tetrisBackgroundColor = { 30, 35, 38, 255 };
+Color borderColor = { 54, 68, 76, 255 };
+float fX(float x) {
+	return x / resolutionDivider;
+}
 
-float playerX = 1280/2, playerY = 720/2, playerWidth = 25, playerHeight = 25, playerRotation = 0, rotationSpeed = 100, moveSpeed = 100;
-float circleX, circleY, circleR = 5;
+// Pieces
+int currentPiece = 0;
+int PIECES[7][8] = {
+	{ // I
+		0, 0, 1, 0, 2, 0, 3, 0
+	},
+	{ // L
+		1, 0, 1, 1, 1, 2, 0, 2
+	},
+	{ // J
+		0, 0, 0, 1, 0, 2, 1, 2
+	},
+	{ // S
+		0, 0, 1, 0, 0, 1, 1, 1
+	},
+	{ // Q
+		1, 0, 2, 0, 0, 1, 1, 1
+	},
+	{ // T
+		1, 0, 0, 1, 1, 1, 2, 1
+	},
+	{ // Z
+		0, 0, 1, 0, 1, 1, 2, 1
+	}
+};
+int PIECE_ROTATIONS[7][8] = {
+	{ // I
+		2, -1, 1, 1, -1, 2, -2, -2
+	},
+	{ // L
+		1, 1, -1, 1, -1, -1, 1, -1
+	},
+	{ // J
+		2, 0, 0, 2, -2, 0, 0, -2
+	},
+	{ // S
+		1, 0, 0, 1, -1, 0, 0, -1
+	},
+	{ // Q
+		1, -1, 1, 2, -2, 0, 0, -1
+	},
+	{ // T
+		2, 0, 0, 2, -2, 0, 0, -2
+	},
+	{ // Z
+		2, -1, 0, 2, -1, 0, -1, -1
+	}
+};
+int PIECE_COLORS[7] = {
+	0, 0, 1, 1, 2, 2, 2
+};
+int currentPieceArray[8];
+int rotateCycle = 0;
+void changePiece(int piece[8]) {
+	for (int i = 0; i < 8; i++) {
+		currentPieceArray[i] = piece[i];
+	}
+}
+
+// Tetris
+float blockWidth = 539 - 49 / 10;
+int x = 4, y = 0;
+float accurateY = 0;
+float fallSpeed = 1.0f;
+float holdSpeed = 10.0f;
+int pieceColor = 0;
+Color pieceColors[3] = {
+	{ 114, 132, 142, 255 },
+	{ 97, 107, 137, 255 },
+	{ 19, 142, 99, 255 }
+};
+int garbage[10][20];
+int testGarbageCollisionManual(int piece[8]) {
+	for (int i = 0; i < 10; i++) {
+		for (int z = 0; z < 20; z++) {
+			if (garbage[i][z] != 0) {
+				for (int j = 0; j < 4; j++) {
+					int drawX = x + (piece[j * 2]), drawY = y + (piece[j * 2 + 1]) - 1;
+					if (drawX == i && drawY + 1 == z) {
+						return -1;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+int testGarbageCollision() {
+	return testGarbageCollisionManual(currentPieceArray);
+}
+int testCollision(int piece[8]) {
+	for (int i = 0; i < 4; i++) {
+		int drawX = x + (piece[i * 2]), drawY = y + (piece[i * 2 + 1]);
+		if (drawX < 0 || drawX > 9) { return -1; }
+	}
+	if (testGarbageCollisionManual(piece) == -1) {
+		return -1;
+	}
+	return 0;
+}
+int testCollisionY(int piece[8]) {
+	for (int i = 0; i < 4; i++) {
+		int drawX = x + (piece[i * 2]), drawY = y + (piece[i * 2 + 1]);
+		if (drawY > 19) { return -1; }
+	}
+	if (testGarbageCollision() == -1) {
+		return -1;
+	}
+	return 0;
+}
+int moveX(int direction) {
+	x += direction;
+	if (testCollision(currentPieceArray) == -1) {
+		x -= direction; return -1;
+	}
+	return 0;
+}
+int moveY(float deltaTime, float _fallSpeed) {
+	accurateY += (_fallSpeed)*deltaTime;
+	y = (int)accurateY;
+	return 0;
+}
+int rotate(int direction) {
+	x += PIECE_ROTATIONS[currentPiece][rotateCycle * 2];
+	accurateY += PIECE_ROTATIONS[currentPiece][rotateCycle * 2 + 1];
+	y = (int)accurateY;
+	int duplicateArray[8];
+	if (direction > 0) {
+		for (int i = 0; i < 4; i++) {
+			duplicateArray[i * 2] = currentPieceArray[i * 2 + 1];
+			duplicateArray[i * 2 + 1] = -currentPieceArray[i * 2];
+		}
+	}
+	else {
+		for (int i = 0; i < 4; i++) {
+			duplicateArray[i * 2] = -currentPieceArray[i * 2 + 1];
+			duplicateArray[i * 2 + 1] = currentPieceArray[i * 2];
+		}
+	}
+	if (testCollision(duplicateArray) == -1) {
+		x -= PIECE_ROTATIONS[currentPiece][rotateCycle * 2];
+		accurateY -= PIECE_ROTATIONS[currentPiece][rotateCycle * 2 + 1];
+		y = (int)accurateY;
+		return -1;
+	}
+	else {
+		for (int i = 0; i < 8; i++) {
+			currentPieceArray[i] = duplicateArray[i];
+		}
+		if (rotateCycle + 1 < 4) {
+			rotateCycle += 1;
+		}
+		else {
+			rotateCycle = 0;
+		}
+		return 0;
+	}
+}
+void checkLines() {
+	int lineCounts[20] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	for (int i = 0; i < 10; i++) {
+		for (int z = 0; z < 20; z++) {
+			if (garbage[i][z] != 0) {
+				lineCounts[z]++;
+			}
+		}
+	}
+	for (int i = 0; i < 20; i++) {
+		if (lineCounts[i] >= 10) {
+			for (int z = 0; z < 10; z++) {
+				garbage[i][z] = 0;
+			}
+			for (int z = i - 1; z >= 0; z--) {
+				for (int p = 0; p < 10; p++) {
+					garbage[p][z + 1] = garbage[p][z];
+				}
+			}
+		}
+	}
+}
 
 int main() {
-	initGrey(0);
+	initGrey(4);
+	initArey();
 
-	Window win = createWindow(1280, 720, "greytracing");
-	
-	int x[size] = {
-		0, 0, 1280-50, 0, 200, 200, 200, 400, 400, 600, 600, 800
-	};
-	int y[size] = {
-		0, 0, 0, 720-50, 250, 250, 450, 450, 600, 450, 100, 100
-	};
-	int w[size] = {
-		1280, 50, 50, 1280, 250, 50, 250, 50, 200, 50, 50, 50
-	};
-	int h[size] = {
-		50, 720, 720, 50, 50, 250, 50, 200, 50, 200, 200, 550
-	};
-	
+	float offsetX = 0.0f, offsetY = 0.0f;
+
+	Window win = createWindow(width / resolutionDivider, height / resolutionDivider, "grey");
+	srand(win.startTime * 1000);
+
+	FontID eightBitDragon = loadFont(&win, "resources/EightBitDragon.ttf", 50);
+
+	currentPiece = rand() % 7;
+	changePiece(PIECES[currentPiece]);
+
 	while (!shouldWindowClose(win)) {
 		updateWindow(&win);
 
-		clearWindowBackground(&win, COLOR_WHITE);
-
-		for (int i = 0; i < size; i++) {
-			drawRectangle(&win, x[i], y[i], w[i], h[i], 0, COLOR_BLACK);
+		srand(win.time * 1000);
+		checkLines();
+		moveY(win.deltaTime, fallSpeed);
+		if (isKeyDown(win, KEY_S) || isKeyDown(win, KEY_DOWN)) { moveY(win.deltaTime, holdSpeed); }
+		if (testCollisionY(currentPieceArray) == -1) {
+			for (int i = 0; i < 4; i++) {
+				int drawX = x + (currentPieceArray[i * 2]), drawY = y + (currentPieceArray[i * 2 + 1]) - 1;
+				garbage[drawX][drawY] = PIECE_COLORS[currentPiece] + 1;
+			}
+			x = 4;
+			accurateY = 0.0f;
+			y = 0;
+			rotateCycle = 0;
+			currentPiece = rand() % 7;
+			changePiece(PIECES[currentPiece]);
 		}
 
-		float px = playerX + playerWidth, py = playerY + (playerHeight / 2);
-		float ox = playerX + (playerWidth / 2), oy = playerY + (playerHeight / 2);
-		float theta = playerRotation * (3.14159265359 / 180);
+		if (win.width / width < win.height / height) {
+			resolutionDivider = width / win.width;
+			offsetX = 0.0f;
+		}
+		else {
+			resolutionDivider = height / win.height;
+			offsetX = -((width / resolutionDivider) - win.width) / 2;
+		}
+		blockWidth = (539 - 49) / 10;
 
-		circleX = cos(theta) * (px - ox) - sin(theta) * (py - oy) + ox;
-		circleY = sin(theta) * (px - ox) + cos(theta) * (py - py) + oy;
+		clearWindowBackground(win, backgroundColor);
 
-		// Controls
-		if (isKeyDown(win, KEY_LEFT)) {
-			playerRotation -= rotationSpeed * win.deltaTime;
-		}
-		if (isKeyDown(win, KEY_RIGHT)) {
-			playerRotation += rotationSpeed * win.deltaTime;
-		}
-		if (isKeyDown(win, KEY_UP)) {
-			playerX += (moveSpeed * cos(theta)) * win.deltaTime;
-			playerY += (moveSpeed * sin(theta)) * win.deltaTime;
-		}
-		if (isKeyDown(win, KEY_DOWN)) {
-			playerX -= (moveSpeed * cos(theta)) * win.deltaTime;
-			playerY -= (moveSpeed * sin(theta)) * win.deltaTime;
+		if (isKeyDown(win, KEY_ESCAPE)) {
+			closeWindow(win);
 		}
 
-		drawRectangle(&win, playerX, playerY, playerWidth, playerHeight, playerRotation, COLOR_YELLOW);
-		drawCircle(&win, circleX, circleY, circleR, COLOR_RED);
+		if (isKeyPressed(win, KEY_F)) {
+			printf("%f\n", win.framesPerSecond);
+		}
+
+		if (isKeyPressed(win, KEY_F11)) {
+			win.fullscreen = !win.fullscreen;
+		}
+
+		//setWireframeMode(win, isKeyDown(win, KEY_SPACE));
+
+		// Input
+		if (isKeyPressed(win, KEY_A) || isKeyPressed(win, KEY_LEFT)) { moveX(-1); }
+		if (isKeyPressed(win, KEY_D) || isKeyPressed(win, KEY_RIGHT)) { moveX(1); }
+		if (isKeyPressed(win, KEY_W) || isKeyPressed(win, KEY_UP)) { rotate(-1); }
+
+		// Draw UI
+		/// Borders
+		drawRectangle(&win, offsetX + fX(36), fX(36), fX(552 - 36), fX(1042 - 36), 0, borderColor);
+		drawRectangle(&win, offsetX + fX(576), fX(85), fX(846 - 576), fX(230 - 85), 0, borderColor);
+		drawRectangle(&win, offsetX + fX(576), fX(330), fX(846 - 576), fX(601 - 330), 0, borderColor);
+		drawRectangle(&win, offsetX + fX(576), fX(919), fX(846 - 576), fX(1042 - 919), 0, borderColor);
+		/// Tetris background
+		drawRectangle(&win, offsetX + fX(49), fX(49), fX(539 - 49), fX(1029 - 49), 0, tetrisBackgroundColor);
+		drawRectangle(&win, offsetX + fX(589), fX(98), fX(833 - 589), fX(217 - 98), 0, tetrisBackgroundColor);
+		drawRectangle(&win, offsetX + fX(589), fX(343), fX(833 - 589), fX(588 - 343), 0, tetrisBackgroundColor);
+		drawRectangle(&win, offsetX + fX(589), fX(932), fX(833 - 589), fX(1029 - 932), 0, tetrisBackgroundColor);
+
+		// Draw garbage
+		for (int i = 0; i < 10; i++) {
+			for (int z = 0; z < 20; z++) {
+				if (garbage[i][z] != 0) {
+					drawRectangle(&win, offsetX + fX(49 + (i * blockWidth)), fX(49 + (z * blockWidth)), fX(blockWidth), fX(blockWidth), 0, pieceColors[garbage[i][z] - 1]);
+				}
+			}
+		}
+		// Draw piece
+		for (int i = 0; i < 4; i++) {
+			int drawX = x + (currentPieceArray[i * 2]), drawY = y + (currentPieceArray[i * 2 + 1]);
+			drawRectangle(&win, offsetX + fX(49 + (drawX * blockWidth)), fX(49 + (drawY * blockWidth)), fX(blockWidth), fX(blockWidth), 0, pieceColors[PIECE_COLORS[currentPiece]]);
+		}
+
+		float scale = 153 - 113;
+		while (813 - (607 + getWidthOfText(&win, "Score: 0", eightBitDragon, scale)) > 0) {
+			scale += 1;
+		}
+		while (813 - (607 + getWidthOfText(&win, "Score: 0", eightBitDragon, scale)) < 0) {
+			scale -= 1;
+		}
+
+		drawBorderedText(&win, "Score: 0", eightBitDragon, offsetX + fX(611), fX(112), fX(scale), fX(2), borderColor, backgroundColor);
 
 		renderWindow(win);
 	}
 
 	deleteWindow(&win);
+
+	closeArey();
 	closeGrey();
+	return 0;
 }
