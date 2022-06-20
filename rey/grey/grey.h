7430d5e9ca2f9b13b00133156c3a1706129f1724
wk1093 <wyattk1093@gmail.com>
2022-06-20 09:14:20 -0700

    getTextHeight and ui.h text on buttons

--- rey/grey/grey.h
+++ rey/grey/grey.h
@@ -384,6 +384,8 @@ extern "C" {
 
 	float getWidthOfText(Window* win, const char* text, FontID font, float scale);
 
+	float getHeightOfText(Window* win, const char* text, FontID font, float scale);
+
 	void drawPolygon(Window* win, float* xs, float* ys, int points, Color color);
 
 	void drawAdvancedRect(Window* win, float x, float y, float width, float height, float rotation, Color color1, Color color2, Color color3, Color color4);
