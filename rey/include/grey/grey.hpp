a9efcd0c7e472f0b71f9fd9f32adde4654100d9d
wk1093 <wyattk1093@gmail.com>
2022-08-12 18:24:42 -0700

    ERRO COMMOIT

--- rey/include/grey/grey.hpp
+++ rey/include/grey/grey.hpp
@@ -1,3 +1,55 @@
 #pragma once
 #define GREY_ENABLE_NAMESPACE
 #include <grey/grey.h>
+
+using grey::initGrey;
+using grey::closeGrey;
+using grey::Color;
+
+class Window {
+public:
+    grey::Window* win;
+    Window(int width, int height, const char* title) {
+        grey::Window window = grey::createWindow(width, height, title);
+        this->win = &window;
+    }
+    ~Window() {
+        grey::deleteWindow(this->win);
+    }
+    explicit operator bool() { 
+        return grey::shouldWindowClose(*this->win); 
+    }
+    void update() {
+        grey::updateWindow(this->win);
+    }
+    void render() {
+        grey::renderWindow(*this->win);
+    }
+    void del() {
+        grey::deleteWindow(this->win);
+    }
+    bool isKeyDown(int key) {
+        return grey::isKeyDown(*this->win, key);
+    }
+    bool isKeyPressed(int key) {
+        return grey::isKeyPressed(*this->win, key);
+    }
+    void close() {
+        grey::closeWindow(*this->win);
+    }
+    void setFlag(uint32_t flag, bool state) {
+        grey::setWindowFlag(*this->win, flag, state);
+    }
+    void clearColor(Color color) {
+        grey::clearWindowBackground(this->win, color);
+    }
+    void setWireframe(bool state) {
+        grey::setWireframeMode(*this->win, state);
+    }
+    void drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3, Color color=COLOR_WHITE) {
+        grey::drawTriangle(this->win, x1, y1, x2, y2, x3, y3, color);
+    }
+    void drawRectangle(float x, float y, float width, float height, float rotation=0, Color color=COLOR_WHITE) {
+        grey::drawRectangle(this->win, x, y, width, height, rotation, color);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,38 @@
 #include <arey/arey.hpp>
-#include <grey/grey.h>
-#include <grey/grey/obj.hpp>
+#include <grey/grey.hpp>
 
 int main() {
 	initGrey(0);
 	initArey();
 
-	Window win = createWindow(1280, 720, "grey");
+	Window win(1280, 720, "grey");
 	Sound aeh = loadSound("resources/aeh.flac");
 	aeh.loopSound = false;
 	aeh.position[0] = -1000;
 	aeh.position[1] = 0;
 	aeh.position[2] = 0;
 
-	while (!shouldWindowClose(win)) {
-		updateWindow(&win);
+	while (win) {
+		win.update();
 
-		if (isKeyDown(win, KEY_LEFT)) {
+		if (win.isKeyDown(KEY_LEFT)) {
 			aeh.position[0] -= 5;
 			
 		}
 
-		if (isKeyDown(win, KEY_RIGHT)) {
+		if (win.isKeyDown(KEY_RIGHT)) {
 			aeh.position[0] += 5;
 		}
 
-		if (isKeyPressed(win, KEY_A)) {
+		if (win.isKeyPressed(KEY_A)) {
 			playSound(&aeh);
 		}
 
-		renderWindow(win);
+		win.render();
 	}
 
 	deleteSound(&aeh);
-	deleteWindow(&win);
+	win.del();
 	closeGrey();
 	closeArey();
 }
